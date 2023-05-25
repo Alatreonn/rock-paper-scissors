d0bfb96f4393b8bd52cd705aa401b90c456a8abb
Alatreonn <127147130+Alatreonn@users.noreply.github.com>
2023-05-25 12:28:11 +0300

    Convert to 5 rounds. Add score.

--- script.js
+++ script.js
@@ -21,9 +21,33 @@ function playRound(playerSelection, computerSelection) {
     return outcomes[playerSelection][computerSelection];
 }
 
-const computerSelection = getComputerChoice();
-console.log(computerSelection);
-const playerSelection = prompt("Rock, Paper or Scissors?").toUpperCase();
-console.log(playerSelection);
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    let drawScore = 0;
 
-console.log(playRound(playerSelection, computerSelection));
+    for (let i = 0; i < 5; i++) {
+        const computerSelection = getComputerChoice();
+        const playerSelection = prompt("Rock, Paper or Scissors?").toUpperCase();
+
+        const result = playRound(playerSelection, computerSelection);
+
+        if (result === "WIN") {
+            playerScore++;
+            console.log(`It's a WIN! ${playerSelection} wins over ${computerSelection}!`);
+        } else if (result === "LOSS") {
+            computerScore++;
+            console.log(`It's a LOSS! ${playerSelection} losses to ${computerSelection}!`);
+        } else {
+            drawScore++
+            console.log(`It's a DRAW! ${playerSelection} is equal to ${computerSelection}!`);
+        }
+    }
+
+    console.log("Final Scores:");
+    console.log(` Player:` + playerScore);
+    console.log(` Computer: ` + computerScore);
+    console.log(` Draws ` + drawScore);
+}
+
+game();
